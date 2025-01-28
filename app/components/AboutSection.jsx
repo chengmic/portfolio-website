05d298dd93c4ce98d89c08e4d295d06397461385
Michelle Cheng <chengmic@oregonstate.edu>
2025-01-27 17:12:47 -0800

    update about

--- app/components/AboutSection.jsx
+++ app/components/AboutSection.jsx
@@ -5,7 +5,7 @@ const AboutSection = () => {
         <div className="mt-4 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base lg:text-lg">
-          In December 2023, I received a B.S. in Computer Science at Oregon State University. When I&apos;m not coding, I enjoy drawing and playing video games. I am currently seeking employment as a junior software engineer.
+          In December 2023, I received a B.S. in Computer Science at Oregon State University. When I&apos;m not programming, I enjoy dancing with my hoop and playing video games. I am currently seeking employment as a junior software engineer.
           </p>
         </div>
       </div>
